e27d20e93e430f0c51798e6f8411556a15069a70
Shokhrukh <uphill7165@gmail.com>
2024-09-21 16:48:44 +0900

    added image picker using firebase

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,39 +1,108 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector} from "react-redux"; // Use dispatch to update avatar
+import { app } from "../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 export default function Profile() {
   const currentUser = useSelector((state) => state.user);
   const { avatar } = currentUser.user.currentUser;
 
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [uploadPer, setUploadPer] = useState(0);
+  const [fileError, setFileError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(file);
+  console.log(fileError);
+  console.log(uploadPer);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPer(Math.round(progress));
+      },
+      (error) => {
+        setFileError(true);
+        console.error("Error during file upload:", error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at:", downloadURL);
+          setFormData({ ...formData, avatar: downloadURL });
+          setUploadPer(100); 
+        });
+      }
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7 ">My Profile</h1>
+      <h1 className="text-3xl font-semibold text-center my-7">My Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])} // Handle file change
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
-          src={avatar}
-          alt=""
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || avatar}
+          alt="Profile Avatar"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
+        <p className="text-sm self-center">
+          {fileError ? (
+            <span className="text-red-700 ">Error occurred</span>
+          ) : uploadPer > 0 && uploadPer < 100 ? (
+            <span className="text-slate-700">{`Uploading ${uploadPer}%`}</span>
+          ) : uploadPer === 100 ? (
+            <span className="text-green-700">Uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
           id="username"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
         />
         <input
           type="email"
           placeholder="email"
           id="email"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
         />
         <input
           type="password"
           placeholder="password"
           id="password"
-          className="border p-3 rounded-lg "
+          className="border p-3 rounded-lg"
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">
-          update
+          Update
         </button>
       </form>
       <div className="flex justify-between mt-5">
