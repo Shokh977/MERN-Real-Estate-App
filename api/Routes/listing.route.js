482ef38378dda834e739ac0c3614c9859e3e3b7e
Shokhrukh <uphill7165@gmail.com>
2024-11-02 22:30:48 +0900

    complete user listing update functionality

--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+
 
 module.exports = router;
