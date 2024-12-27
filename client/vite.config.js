1f2f5bdc78d096b452a9c399405966deeb9dcbe7
Shokhrukh <uphill7165@gmail.com>
2024-12-27 21:31:20 +0900

    ...

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mern-real-estate-app-2nqr.onrender.com", // Ensure this URL is correct
-        secure: false, // Set to false if you're using HTTP on your backend
+        // secure: false, // Set to false if you're using HTTP on your backend
       },
     },
   },
