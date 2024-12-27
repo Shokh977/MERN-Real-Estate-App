22fb58341ad5f9445c4f6a6fbb8a2c0888eb821d
Shokhrukh <uphill7165@gmail.com>
2024-12-27 21:15:04 +0900

    update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc"; 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "mern-real-estate-app-2nqr.onrender.com",
-        secure: false,
+        target: "https://mern-real-estate-app-2nqr.onrender.com", // Ensure this URL is correct
+        secure: false, // Set to false if you're using HTTP on your backend
       },
     },
   },
